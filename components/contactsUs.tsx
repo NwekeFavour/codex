cf098136f610b96c27521c47ed608dbc42679006
NwekeFavour <buchijunior90@gmail.com>
2025-09-11 16:38:41 +0100

    86%

--- components/contactsUs.tsx
+++ components/contactsUs.tsx
@@ -43,7 +43,7 @@ export default function QuickContactSection() {
   }
 
   return (
-    <section className="py-20 bg-gradient-to-br px-10 from-indigo-50 to-blue-50">
+    <section className="py-20 bg-gradient-to-br md:px-10 from-indigo-50 to-blue-50">
       <div className="container mx-auto px-4">
         <div className="max-w-4xl mx-auto">
           <div className="text-center mb-12">
