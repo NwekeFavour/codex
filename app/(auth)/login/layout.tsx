a003121f0c1b578ad8da0099647f4137ca5dc69a
NwekeFavour <buchijunior90@gmail.com>
2025-09-27 13:12:28 +0100

    prosp

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -8,7 +8,7 @@ export default function LoginLayout({ children }: { children: React.ReactNode })
     const script = document.createElement("script")
     script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`
     script.async = true
-    script.defer = true
+    script.defer = true   
     document.body.appendChild(script)
 
     return () => {
